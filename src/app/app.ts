abd2951cc21e3aeabc846edf21f97213306f3567
junior <jr.siilva@outlook.com>
2025-10-02 19:49:52 -0300

    First commit

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,25 @@
 import { Component, signal } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+
+import { Header } from './header/header';
+import { User } from './user/user';
+import { DUMMY_USERS } from './user/dummy-users';
+import { Tasks } from './tasks/tasks';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Header, User, Tasks],
   templateUrl: './app.html',
-  styleUrl: './app.css'
+  styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('project1');
+  selectedUserId = 'u1';
+  users = DUMMY_USERS;
+
+  get selectedUser() {
+    return this.users.find((user) => user.id === this.selectedUserId)!;
+  }
+
+  onSelectUser(id: string) {
+    this.selectedUserId = id;
+  }
 }
---
+++ src/app/user/user.ts
@@ -0,0 +1,22 @@
+import { Component, computed, EventEmitter, Input, input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-user',
+  imports: [],
+  templateUrl: './user.html',
+  styleUrl: './user.css',
+})
+export class User {
+  @Input({ required: true }) id!: string;
+  @Input({ required: true }) avatar!: string;
+  @Input({ required: true }) name!: string;
+  @Output() select = new EventEmitter<string>();
+
+  get imagePath() {
+    return 'assets/users/' + this.avatar;
+  }
+
+  onSelectedUser() {
+    this.select.emit(this.id);
+  }
+}
